5609f840c8b1e94e52b3904601550de12dc97f57
David Cervera Martí <davidmarti.mensajes@gmail.com>
2016-12-26 21:01:39 +0100

    Added left animations.
    Added left animations of Michelangelo.

--- SDL Game 3 - Master - Handout/ModulePlayer.cpp
+++ SDL Game 3 - Master - Handout/ModulePlayer.cpp
@@ -12,7 +12,15 @@
 
 ModulePlayer::ModulePlayer(bool active) : Module(active)
 {
-	// idle animation
+	// idle animation right
+	idle_left.frames.push_back({ 753, 30, 50, 60 });
+	idle_left.frames.push_back({ 671, 30, 50, 60 });
+	idle_left.frames.push_back({ 589, 30, 50, 60 });
+	idle_left.frames.push_back({ 507, 30, 50, 60 });
+	idle_left.loop = true;
+	idle_left.speed = 0.05f;
+
+	// idle animation left
 	idle_right.frames.push_back({ 21, 30, 50, 60 });
 	idle_right.frames.push_back({ 103, 30, 50, 60 });
 	idle_right.frames.push_back({ 185, 30, 50, 60 });
@@ -33,6 +41,18 @@ ModulePlayer::ModulePlayer(bool active) : Module(active)
 	up_right.loop = true;
 	up_right.speed = 0.15f;
 
+	// move upwards
+	up_left.frames.push_back({ 350, 599, 50, 64 });
+	up_left.frames.push_back({ 267, 599, 50, 64 });
+	up_left.frames.push_back({ 186, 599, 50, 64 });
+	up_left.frames.push_back({ 105, 599, 50, 64 });
+	up_left.frames.push_back({ 21, 599, 50, 64 });
+	up_left.frames.push_back({ 761, 693, 50, 64 });
+	up_left.frames.push_back({ 677, 693, 50, 64 });
+	up_left.frames.push_back({ 595, 693, 50, 64 });
+	up_left.loop = true;
+	up_left.speed = 0.15f;
+
 	// move right
 	right.frames.push_back({596, 504, 50, 68 });
 	right.frames.push_back({ 685, 504, 50, 68 });
@@ -44,6 +64,18 @@ ModulePlayer::ModulePlayer(bool active) : Module(active)
 	right.frames.push_back({ 355, 600, 50, 68 });
 	right.loop = true;
 	right.speed = 0.15f;
+
+	// move left
+	left.frames.push_back({ 181, 504, 50, 68 });
+	left.frames.push_back({ 100, 504, 50, 68 });
+	left.frames.push_back({ 20, 504, 50, 68 });
+	left.frames.push_back({ 759, 600, 50, 68 });
+	left.frames.push_back({ 679, 600, 50, 68 });
+	left.frames.push_back({ 598, 600, 50, 68 });
+	left.frames.push_back({ 514, 600, 50, 68 });
+	left.frames.push_back({ 428, 600, 50, 68 });
+	left.loop = true;
+	left.speed = 0.15f;
 }
 
 ModulePlayer::~ModulePlayer()
@@ -55,6 +87,7 @@ bool ModulePlayer::Start()
 	LOG("Loading player");
 
 	graphics = App->textures->Load("rtype/michelangeloderecha.png");
+	graphics2 = App->textures->Load("rtype/michelangeloizquierda.png");
 
 	destroyed = false;
 	position.x = 150;
@@ -71,6 +104,7 @@ bool ModulePlayer::CleanUp()
 	LOG("Unloading player");
 
 	App->textures->Unload(graphics);
+	App->textures->Unload(graphics2);
 
 	return true;
 }
@@ -115,10 +149,10 @@ update_status ModulePlayer::Update()
 	if(App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT)
 	{
 		position.y -= speed;
-		if(current_animation != &up_right)
+		if(current_animation != &up_left)
 		{
 			up_right.Reset();
-			current_animation = &up_right;
+			current_animation = &up_left;
 		}
 	}
 
@@ -130,12 +164,12 @@ update_status ModulePlayer::Update()
 
 	if(App->input->GetKey(SDL_SCANCODE_S) == KEY_IDLE
 	   && App->input->GetKey(SDL_SCANCODE_W) == KEY_IDLE)
-		current_animation = &idle_right;
+		current_animation = &left;
 
 	// Draw everything --------------------------------------
 	if (destroyed == false)
 	{
-		App->renderer->Blit(graphics, position.x, position.y, &(current_animation->GetCurrentFrame()));
+		App->renderer->Blit(graphics2, position.x, position.y, &(current_animation->GetCurrentFrame()));
 		col->SetPos(position.x, position.y);
 	}
 
--- SDL Game 3 - Master - Handout/ModulePlayer.h
+++ SDL Game 3 - Master - Handout/ModulePlayer.h
@@ -21,10 +21,12 @@ public:
 public:
 
 	SDL_Texture* graphics = nullptr;
+	SDL_Texture* graphics2 = nullptr;
 	Animation* current_animation = nullptr;
 	Animation idle_right;
 	Animation idle_left;
 	Animation up_right;
+	Animation up_left;
 	Animation down;
 	Animation left;
 	Animation right;
